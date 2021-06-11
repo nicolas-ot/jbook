5903a9ab594128d556ba7d8e3387ddfbf9eff40e
2001-otter <nicolastheodarus@gmail.com>
2021-06-11 13:51:28 +0200

    initial commit

---
+++ src/plugins/unpkg-path-plugin.ts
@@ -0,0 +1,64 @@
+import * as esbuild from "esbuild-wasm";
+import axios from "axios";
+import localForage from "localforage";
+
+const fileCache = localForage.createInstance({
+  name: "filecache",
+});
+
+export const unpkgPathPlugin = (input: string) => {
+  return {
+    name: "unpkg-path-plugin",
+    setup(build: esbuild.PluginBuild) {
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        console.log("onResolve", args);
+
+        if (args.path === "index.js") {
+          return { path: args.path, namespace: "a" };
+        } else if (args.path.includes("./") || args.path.includes("../")) {
+          return {
+            path: new URL(args.resolveDir + "/" + args.path, args.importer)
+              .href,
+            namespace: "a",
+          };
+        } else {
+          return {
+            path: `https://unpkg.com/${args.path}`,
+            namespace: "a",
+          };
+        }
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        console.log("onLoad", args);
+
+        if (args.path === "index.js") {
+          return {
+            loader: "jsx",
+            contents: input,
+          };
+        }
+
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        if (cachedResult) {
+          return cachedResult;
+        }
+
+        const { data, request } = await axios.get(args.path);
+        // console.log(data);
+        // console.log(request);
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+
+        await fileCache.setItem(args.path, result);
+        return result;
+      });
+    },
+  };
+};
